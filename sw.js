8bc19670e60a1a6fbeaf86660b62b4dbf57dc0c9
ahmaderta rizqi <ahmadderta.rizqi30@gmail.com>
2020-07-03 21:03:42 +0700

    add assets img in service worker, actually this is not the best way

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 const CACHE_NAME = 'FIRST_PWA';
 const urlsToCache = [
    "/",
+   "/app_logo_favicon.png",
    "/index.html",
    "/navigation.html",
    "/pages/about.html",
@@ -17,10 +18,31 @@ const urlsToCache = [
    "/img/bg-banner.jpg",
    "/data/movies.json",
    "/data/tvshows.json",
+   "/data/imgs/movies/1Duc3EBiegywczxTWekvy03HWai.jpg",
+   "/data/imgs/movies/f4aul3FyD3jv3v4bul1IrkWZvzq.jpg",
+   "/data/imgs/movies/fOvqEunubL3wPskvtk2Ficfl0pH.jpg",
+   "/data/imgs/movies/nPx3juctkdZvgrJ4DO35dUnVsyF.jpg",
+   "/data/imgs/movies/5xPfTKBa9R6YGcxWpfRzYHjk3IA.jpg",
+   "/data/imgs/movies/3B2twt8I02cLAPHG6dkDpTFbgz5.jpg",
+   "/data/imgs/movies/8WUVHemHFH2ZIP6NWkwlHWsyrEL.jpg",
+   "/data/imgs/movies/oyG9TL7FcRP4EZ9Vid6uKzwdndz.jpg",
+   "/data/imgs/movies/jHo2M1OiH9Re33jYtUQdfzPeUkx.jpg",
+   "/data/imgs/movies/myf3qzpeN0JbuFRPwSpJcz7rmAT.jpg",
+   "/data/imgs/movies/wxPhn4ef1EAo5njxwBkAEVrlJJG.jpg",
+   "/data/imgs/movies/zQFjMmE3K9AX5QrBL1SXIxYQ9jz.jpg",
+   "/data/imgs/tvshows/6t6r1VGQTTQecN4V0sZeqsmdU9g.jpg",
+   "/data/imgs/tvshows/ajmkAwuK1TRFWMjKoSMgoAXbnc7.jpg",
+   "/data/imgs/tvshows/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg",
+   "/data/imgs/tvshows/wcaDIAG1QdXQLRaj4vC1EFdBT2.jpg",
+   "/data/imgs/tvshows/bRJOwllemPbE4JTQ0TtcVu9efff.jpg",
+   "/data/imgs/tvshows/drlfSCIlMKrEeMPhi8pqY4xGxj.jpg",
+   "/data/imgs/tvshows/2AoqUTKKN4kjNe1K0kIPwiItHwv.jpg",
+   "/data/imgs/tvshows/gKG5QGz5Ngf8fgWpBsWtlg5L2SF.jpg",
+   "/data/imgs/tvshows/gHUCCMy1vvj58tzE3dZqeC9SXus.jpg",
+   "/data/imgs/tvshows/1YffzZDZmxonFz4MTy0gaBDC3S0.jpg",
+   "/data/imgs/tvshows/gPBf35AqvXHvKEpDHaQ4D9xXxeX.jpg",
+   "/data/imgs/tvshows/ggFHVNu6YYI5L9pCfOacjizRGt.jpg",
 ];
-
-// akses url from service worker
-let accessURL = self.location.origin;
 
 self.addEventListener('install', function(ev) {
    ev.waitUntil(
