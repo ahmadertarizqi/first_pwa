14e80692b52eb087387eaf18eb1be487ad3fa6b9
ahmaderta rizqi <ahmadderta.rizqi30@gmail.com>
2020-07-04 12:29:39 +0700

    remove comment

--- js/app.js
+++ js/app.js
@@ -110,7 +110,6 @@ const clearLoader = () => {
 
 const randomIndex = (items) => {
    /** 
-    * Kode ini berasal dari stackoverflow, berikut sumbernya : 
     * https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
     * */ 
 
@@ -119,11 +118,9 @@ const randomIndex = (items) => {
       randomIndex;
    
    while(0 !== currentIndex) {
-      // Pick a remaining element...
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
       
-      // And swap it with the current element.
       temporaryValue = items[currentIndex];
       items[currentIndex] = items[randomIndex];
       items[randomIndex] = temporaryValue;
